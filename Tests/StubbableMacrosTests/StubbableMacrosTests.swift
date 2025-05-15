eed0fca37e57f7a860874abf066d3053f705af58
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2025-05-15 20:10:18 +0200

    Fix tests, remove StubbableClient target and product

--- Tests/StubbableMacrosTests/StubbableMacrosTests.swift
+++ Tests/StubbableMacrosTests/StubbableMacrosTests.swift
@@ -11,64 +11,75 @@ let testMacros: [String: Macro.Type] = [
 
 final class StubbableMacrosTests: XCTestCase {
     func testMacro() throws {
-        assertMacroExpansion(
-            """
-            @Stubbable(exclude: ["id"])
-            struct Foo {
-                let id: String
-                let a: Int
-                let b: String
-                let c: Bool
-                let d: URL
-                let e: Date?
-                let f: [Int]
-                let g: Bar?
-                let h: Bar
+        let fooDeclaration = """
+        struct Foo {
+            let a: ExcludedPropertyType
+            let b: Int
+            let c: String
+            let d: Bool
+            let e: URL
+            let f: Date?
+            let g: [Int]
+            let h: CustomType
+            let i: CustomType?
+            let j: Error
+            let k: Array<Int>
+            let l: Set<Int>
+            let m: [String: Bool]
+            let n: Any
+            let o: AnyObject
+        }
+        """
+
+        let source = """
+        @Stubbable(exclude: ["a"])
+        \(fooDeclaration)
+        """
+
+        let expandedSource = """
+        \(fooDeclaration)
+        
+        extension Foo {
+            #if STUBS_ENABLED
+            static func stub(
+                a: ExcludedPropertyType,
+                b: Int = 0,
+                c: String = "c.\\(UUID().uuidString)",
+                d: Bool = false,
+                e: URL = URL(string: "https://example.com/e/\\(UUID().uuidString)")!,
+                f: Date? = Date(timeIntervalSince1970: 0),
+                g: [Int] = [],
+                h: CustomType = .stub(),
+                i: CustomType? = .stub(),
+                j: Error = NSError(domain: "Foo.j", code: 0, userInfo: nil),
+                k: Array<Int> = [],
+                l: Set<Int> = [],
+                m: [String: Bool] = [:],
+                n: Any,
+                o: AnyObject
+            ) -> Foo {
+                Foo(
+                    a: a,
+                    b: b,
+                    c: c,
+                    d: d,
+                    e: e,
+                    f: f,
+                    g: g,
+                    h: h,
+                    i: i,
+                    j: j,
+                    k: k,
+                    l: l,
+                    m: m,
+                    n: n,
+                    o: o
+                )
             }
-            """,
-            expandedSource:
-            """
-            struct Foo {
-                let id: String
-                let a: Int
-                let b: String
-                let c: Bool
-                let d: URL
-                let e: Date?
-                let f: [Int]
-                let g: Bar?
-                let h: Bar
-            }
-            
-            extension Foo {
-                #if STUBS_ENABLED
-                static func stub(
-                    id: String,
-                    a: Int = 0,
-                    b: String = "b.\\(UUID().uuidString)",
-                    c: Bool = false,
-                    d: URL = URL(string: "https://example.com/d/\\(UUID().uuidString)")!,
-                    e: Date? = Date(timeIntervalSince1970: 0),
-                    f: [Int] = [],
-                    g: Bar? = .stub(),
-                    h: Bar = .stub()
-                ) -> Foo {
-                    Foo(
-                        id: id,
-                        a: a,
-                        b: b,
-                        c: c,
-                        d: d,
-                        e: e,
-                        f: f,
-                        g: g,
-                        h: h
-                    )
-                }
-                #endif
-            }
-            """,
-            macros: testMacros
-        )
+            #endif
+        }
+        """
+
+        assertMacroExpansion(source, expandedSource: expandedSource, macros: testMacros)
     }
 }
